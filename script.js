abf34cf30207a7665869c12690aba4fc72b69403
abdi01dev <abdi01dev@gmail.com>
2024-01-10 17:58:44 +0700

    feat: menambahkan hero

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const toggle = document.querySelector('.sections.section--header .nav__toggle');
 toggle.addEventListener('click', function () {
   nav.classList.toggle('active');
 });
+
+// Header
+const header = document.querySelector('.sections.section--header');
+let currentScrollY = 0;
+let minY = 200;
+window.addEventListener('scroll', e => {
+  currentScrollY = window.scrollY;
+
+  if (currentScrollY >= minY) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+  // currentScrollY >= minY ? header.classList.add('scroll') : header.classList.remove('scroll');
+});
+
+// Body Ddebug
+document.body.addEventListener('dblclick', () => {
+  document.body.classList.toggle('debug');
+});
