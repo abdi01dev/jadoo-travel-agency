607688ac23ef1670e0debdec942742f0d120f4c2
abdi01dev <abdi01dev@gmail.com>
2024-01-13 23:20:52 +0700

    feat: menambahkan section services

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ window.addEventListener('scroll', e => {
 document.body.addEventListener('dblclick', () => {
   document.body.classList.toggle('debug');
 });
+
+// Cards on hover
+const cards = Array.from(document.querySelectorAll('.section--services .card'));
+cards.forEach(card => {
+  card.addEventListener('mouseover', () => {
+    resetCardClass();
+    card.classList.add('active');
+  });
+
+  card.addEventListener('focus', () => {
+    resetCardClass();
+    card.classList.add('active');
+  });
+});
+
+function resetCardClass() {
+  cards.forEach(card => {
+    card.classList.remove('active');
+  });
+}
